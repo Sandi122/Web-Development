c0f0244674632287fbf878e8b80b99a4cab694ba
Sandi122 <singhsandip712@gmail.com>
2021-09-26 19:42:23 +0530

    new projects
    new files

---
+++ Sandip Raj/react_project/restaurant/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Restaurant from './component/basics/Restaurant';
+
+const App = () => {
+    return (
+        <div>
+            
+         <Restaurant/>
+
+        </div>
+    )
+}
+
+
+
+export default App;
---
+++ Sandip Raj/react_project/restaurant/src/component/basics/menuApi.js
@@ -0,0 +1,132 @@
+ const Menu = [
+    {
+    id: 1,
+    image: "images/maggie.webp",
+    name : "Maggie",
+    category: "BreakFast",
+    price : 25,
+    description:
+     " Maggi was the first to bring protein-rich legume meals to the market. It is very tasty ",
+    },
+    
+    {
+    id: 2,
+    image: "images/Poha.jpg",
+    name : "Poha",
+    category: "BreakFast",
+    price : 15,
+    description:
+    " Poha is a very healthy option for breakfast . It is very tasty. ",
+    },
+
+    {
+    id: 3,
+    image: "images/Puri-Sabji.jpg",
+    name : "Puri-Sabji",
+    category: "BreakFast",
+    price : 30,
+    description:
+    " Puri-Sabji is a Bihar delicacy. It is fullfilling breakfast. It is very tasty ",
+    },
+
+    {
+    id: 4,
+    image: "images/veg-thali.jpg",
+    name : "Vag-Thali",
+    category: "Lunch",
+    price : 50,
+    description:
+    " Veg-Thali consist of rice, dal, two types of vegetables and salad. It is very economical. ",
+    }, 
+         
+    {
+    id: 5,
+    image: "images/non-veg-thali.jpg",
+    name : "Non-Veg Thali",
+    category: "Lunch",
+    price : 70,
+    description:
+    " Non-Veg Thali consist of Rice with non-veg option like chicken or fish or mutton ",
+    },  
+    
+    {
+    id: 6,
+    image: "images/chef-special.jpg",
+    name : "Chef-Special",
+    category: "Lunch",
+    price : 80,
+    description:
+    " Customer can demand any type of special dish available in menu, that he want to eat. ",
+    },
+       
+    {
+    id: 7,
+    image: "images/paneer-special.jpg",
+    name : "Paneer special dishes",
+    category: "Dinner",
+    price : 100,
+    description:
+    " Custome can eat any type of paneer related dishes with butter roti or naan. ",
+    },
+
+    {
+    id: 8,
+    image: "images/chicken-special.jpg",
+    name : "Chicken Special Dishes",
+    category: "Dinner",
+    price : 150,
+    description:
+    " Customer can eat any type of paneer related dishes with butter roti or naan. ",
+    },
+
+    {
+    id: 9,
+    image: "images/Mutton-special.jpg",
+    name : "Mutton Special Dishes",
+    category: "Dinner",
+    price : 175,
+    description:
+    " Customer can eat any type of paneer related dishes with butter roti or naan.  ",
+    }, 
+    
+    {
+        id: 10,
+        image: "images/Gulab_jamun.jpg",
+        name : "GulabJamun",
+        category: "Sweets",
+        price : 20,
+        description:
+        " Customer can eat any type of paneer related dishes with butter roti or naan.  ",
+        }, 
+
+    {
+        id: 11,
+        image: "images/samosa.webp",
+        name : "Samosa",
+        category: "Snacks",
+        price : 12,
+        description:
+        " Customer can eat any type of paneer related dishes with butter roti or naan.  ",
+        },  
+
+    {
+        id: 12,
+        image: "images/Rasmalai.jpg",
+        name : "Rasmalai",
+        category: "Sweets",
+        price : 40,
+        description:
+        " Customer can eat any type of paneer related dishes with butter roti or naan.  ",
+        },
+
+    {
+        id: 13,
+        image: "images/Aloo-Tikki-Chaat.jpg",
+        name : "Aalu-Tikki-Chaat",
+        category: "Snacks",
+        price : 30,
+        description:
+        " Customer can eat any type of paneer related dishes with butter roti or naan.  ",
+        },  
+];
+export default Menu;
